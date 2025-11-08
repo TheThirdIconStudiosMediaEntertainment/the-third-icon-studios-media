88aa7752688082bbad25292ad244b40c13eaa358
TheThirdIconStudiosMediaEntertainment <mrigotbars3@gmail.com>
2025-11-08 16:32:00 -0600

    BlockRadio.tsx

--- src/BlockRadio.tsx
+++ src/BlockRadio.tsx
@@ -1,1 +1,10 @@
+function BlockRadio() {
+  return (
+    <div>
+      <h1>The Block Radio</h1>
+      <p>From the streets to the streams — music, money, sports, and what’s real.</p>
+    </div>
+  )
+}
 
+export default BlockRadio
